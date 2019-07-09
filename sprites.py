7f0ff773e2703c511a0edea776d94767e6d65d97
Rafael Said <rafasaid@gmail.com>
2019-07-09 10:27:46 -0300

    Incluído o cenário e os objetos do jogo

--- sprites.py
+++ sprites.py
@@ -11,3 +11,16 @@ helicopter_damaged_2 = pygame.image.load('sprites/helicopter_damage_2.png')
 enemy_helicopter_1 = pygame.image.load('sprites/enemy_helicopter_1.png')
 enemy_helicopter_2 = pygame.image.load('sprites/enemy_helicopter_2.png')
 boat = pygame.image.load('sprites/boat.png')
+
+helicopter_list = [helicopter_1, helicopter_2]
+damage_helicopter_list = [helicopter_damaged_1, helicopter_damaged_2]
+enemy_helicopter_list = [enemy_helicopter_1, enemy_helicopter_2]
+balloon = pygame.image.load('sprites/balloon.png')
+spaceship = pygame.image.load('sprites/balloon.png')
+icon = pygame.image.load('sprites/icon.png')
+background = pygame.image.load('sprites/background.png')
+cloud = pygame.image.load('sprites/cloud.png')
+
+all_sprites = [helicopter_1, helicopter_2, helicopter_damaged_1, helicopter_damaged_2, enemy_helicopter_1,
+               enemy_helicopter_2, helicopter_crash_1, helicopter_crash_2, helicopter_crash_3, helicopter_crash_4, boat,
+               balloon, spaceship, icon, background, cloud]
